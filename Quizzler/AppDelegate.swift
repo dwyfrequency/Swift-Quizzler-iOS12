53964fcbf3d15bdb4d87b0148791ab907227fa72
dwyfrequency <jdwy215@gmail.com>
2019-07-28 22:31:15 -0400

    adding constant ignore

--- Quizzler/AppDelegate.swift
+++ Quizzler/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        print(Secrets().apiKey)
+        print(Secrets())
         
         // Override point for customization after application launch.
         return true
