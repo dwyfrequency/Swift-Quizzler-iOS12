8318f5c772a272ff564ed30e29f697fd86f3b86d
dwyfrequency <jdwy215@gmail.com>
2019-07-28 22:27:22 -0400

    adding files

--- Quizzler/AppDelegate.swift
+++ Quizzler/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-  
+        print(Secrets().apiKey)
         
         // Override point for customization after application launch.
         return true
